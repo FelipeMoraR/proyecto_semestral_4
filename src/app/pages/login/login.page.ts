fee5b591b194fe27d113e0c464f994abd2d1c395
ElPirocas <gnar2342@gmail.com>
2021-09-08 21:10:03 -0300

    botones y alertas
    si

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -7,11 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  variable1 = "";
+  password1 = "";
+  comparativa_v1 ="xXeduardoXx";
+  comparativa_p1="123";
 
-  constructor() { }
+  comparativa_v2 ="Nicolipe";
+  comparativa_p2="1234";
+
+  comparativa_v3 ="Morao";
+  comparativa_p3="12345";
+  
+
+  constructor(private router: Router,public alertController: AlertController) { }
 
   ngOnInit() {
   }
 
+  async validarInicio(){
+    if(this.variable1 == this.comparativa_v1 || this.variable1 == this.comparativa_v2 || this.variable1 == this.comparativa_v3 
+      && this.password1 == this.comparativa_p1 || this.password1 == this.comparativa_p2 || this.password1 == this.comparativa_p3 ){
+      console.log("pancito")
+      this.router.navigate(['/home'])
+      return
+
+    }
+    console.log("no es pancito")
+    const alert = await this.alertController.create({
+      header: "Error al iniciar sesion",
+      message: "Verifique sus datos",
+      buttons: ["OK"]
+    });
+    await alert.present()
+    let result = await alert.onDidDismiss();
+    console.log(result);
+
+  }
   
 }
