e652f56517686399cc4426118f063f751f45ffb2
ponytouwu <ni.cabrerac@duocuc.cl>
2021-10-27 19:19:21 -0300

    XDD

--- src/app/pages/listarp/listarp.page.ts
+++ src/app/pages/listarp/listarp.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listarp',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarpPage implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router) { }
 
   ngOnInit() {
   }
+
+
+
+
+agregarpro(){
+  this.router.navigate(['/agregarpro'])
+}
 
 }
